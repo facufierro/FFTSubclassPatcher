e6112076b2270a258f345f791c0c8db9cfa13b0c
Facundo Fierro <fierrof_47@hotmail.com>
2023-09-02 11:17:58 +0200

    test

--- model/bg3_mod.py
+++
@@ -1,9 +0,0 @@
-# bg3_mod.py
-
-class Mod:
-    def __init__(self, uuid, name, author, folder, prgoressions=None):
-        self.uuid = uuid
-        self.name = name
-        self.author = author
-        self.folder = folder
-        self.progressions = prgoressions if prgoressions else []
